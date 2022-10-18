06554f579e5ae63662b22bfeabb8f87137c1b70b
Akash Prasher <prince.akash.prasher@gmail.com>
2022-10-18 10:57:12 +0530

    tut: TypeScript Tutorial #2 - Compiling TypeScript
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var chars = 'repository';
+console.log(chars);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
+// 'tsc sandbox.ts -w' can be used to compile ts autimatically.
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+const chars = 'repository';
+console.log(chars);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+    console.log(input);
+})
+
+// 'tsc sandbox.ts -w' can be used to compile ts autimatically.
